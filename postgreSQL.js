908fdf9bdfa0c0768f49cd877eb2987e16380b99
Michael Zhang <99362238+icewkz@users.noreply.github.com>
2023-10-02 21:12:19 -0400

    Update postgreSQL.js
    deleted commented SQL URI

--- postgreSQL.js
+++ postgreSQL.js
@@ -4,8 +4,7 @@ const { Pool } = pkg;
 
 
 const PG_URI = process.env.SQL_DB;
-// const PG_URI =
-//    'postgres://sgobvnzo:R2qxsLpz8OTMb43lyGtifXtLVkmSbpJn@berry.db.elephantsql.com/sgobvnzo';
+
 
 // create a new pool here using the connection string above
 const pool = new Pool({
